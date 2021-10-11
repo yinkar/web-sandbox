bb41eea3d507ec773e856632d6848df0bda2e72a
yinkar <hkrabs@yandex.com>
2021-10-11 03:08:31 +0300

    Initialization

---
+++ main.js
@@ -0,0 +1,35 @@
+
+const htmlCodeArea = document.querySelector('.html-code');
+const cssCodeArea = document.querySelector('.css-code');
+const jsCodeArea = document.querySelector('.js-code');
+
+const preview = document.querySelector('.preview');
+
+let pageCode = '';
+
+function updatePreview() {
+
+    pageCodeRaw = [
+        unescape('%3C%21DOCTYPE%20html%3E%0A%3Chtml%3E%0A%3Chead%3E%0A%09%3Ctitle%3E%3C/title%3E%0A%0A%09%3Cstyle%3E%0A'),
+        cssCodeArea.innerText,
+        unescape('%0A%09%3C/style%3E%0A%3C/head%3E%0A%3Cbody%3E'),
+        htmlCodeArea.innerText,
+        unescape('%0A%09%3Cscript%3E'),
+        jsCodeArea.innerText,
+        unescape('%0A%09%3C/script%3E%0A%3C/body%3E%0A%3C/html%3E')
+    ];
+
+    pageCode = pageCodeRaw.join('');
+
+    preview.srcdoc = pageCode;
+};
+
+htmlCodeArea.addEventListener('input', () => {
+    updatePreview();
+});
+cssCodeArea.addEventListener('input', () => {
+    updatePreview();
+});
+jsCodeArea.addEventListener('input', () => {
+    updatePreview();
+});
